a9d56705d573337719ed888fbcd7f0c6d2d9807b
Alaisha Sharma <alaisha_sharma@college.harvard.edu>
2020-06-03 01:11:58 -0700

    jobid and j calculated at runtime in fasrc script

---
+++ mdp/fasrc_generate_scripts.py
@@ -0,0 +1,47 @@
+import argparse
+import sys
+
+from pathlib import Path
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description="generate one script per job to collect MDP data")
+    parser.add_argument("-p", "--paramslist", help="txt file with newline separated list of paramsfiles")
+    parser.add_argument("-c", "--cores", help="allotted cores on single machine")
+    parser.add_argument("-h", "--hrs", help="total allotted runtime in hours")
+    parser.add_argument("-g", "--gigs", help="allotted memory in GB")
+    args = parser.parse_args()
+
+    fasrc_dir = Path("fasrc/")
+    pf = fasrc_dir / "{}.txt".format(args.paramslist)
+    with open(pf, 'r') as paramsfile:
+        params_list = paramsfile.readlines()
+    paramsfile.close()
+    params_list = [p.strip() for p in params_list]
+
+    for paramsfile in params_list:
+        script_dir = Path("fasrc/scripts")
+        sf = script_dir / "run_{}.txt".format(paramsfile)
+        with open(sf, 'w+') as scriptfile:
+            scriptfile.write('''\
+                             #!/bin/bash
+                             #SBATCH -n {}                  # Number of cores (-n)
+                             #SBATCH -N 2                   # Ensure that all cores are on one Node (-N)
+                             #SBATCH -t 0-{}:00             # Runtime in D-HH:MM, minimum of 10 minutes
+                             #SBATCH -p tambe               # Partition to submit to
+                             #SBATCH --mem={}000            # Memory pool for all cores (see also --mem-per-cpu)
+                             #SBATCH -o myoutput_%j.out     # File to which STDOUT will be written, using jobid
+                             #SBATCH -e myerrors_%j.err     # File to which STDERR will be written, using j
+                             module load Anaconda3/2019.10
+                             python fill_mdp_fh_data.py -p {}
+                             '''.format(args.cores,
+                                        args.hrs,
+                                        args.gigs,
+                                        args.jobID,
+                                        args.j,
+                                        paramsfile))
+        scriptfile.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
